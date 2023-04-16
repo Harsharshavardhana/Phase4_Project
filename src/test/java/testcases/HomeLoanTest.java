bb59eeee60b5c0e7d2b304a67cb1003a8cd73c9d
Your Name <you@example.com>
2023-04-16 10:37:29 +0530

    saving TestNg file

---
+++ src/test/java/testcases/HomeLoanTest.java
@@ -0,0 +1,21 @@
+package testcases;
+
+
+import org.testng.annotations.Test;
+
+public class HomeLoanTest extends BaseClass{
+	
+
+	
+	@Test (dependsOnMethods="test2")
+	public void test1() {
+		System.out.println("Inside home loan test 1");
+	}
+		
+	@Test
+	public void test2() {
+		System.out.println("Inside home loan test 2");
+	}
+		
+		
+}
